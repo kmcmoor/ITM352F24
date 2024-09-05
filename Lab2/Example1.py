9eebd415fce3a09ef38f2f6f5976d450d7e542c1
kmcmoor <kmcmoor@hawaii.edu>
2024-09-04 21:40:00 -0700

    Lab 2

---
+++ Lab2/Example1.py
@@ -0,0 +1,15 @@
+# This program is to explore data types
+# Author: Kimberly McMoore
+# 09/04/24
+
+trip_id = "s8adiofh90"
+print("The type of trip_id is:", type (trip_id))
+
+trip_seconds = 300;
+print("The type of trip seconds is:", type(trip_seconds))
+
+trip_fare = 6.75;
+print("The type of trip_fare is:", type(trip_fare))
+
+trip_complete = True;
+print("The type of trip_complete is:", type(trip_complete))
