78ef7601a5ea16ad44167eea91592bcb14864c1e
kmcmoor <kmcmoor@hawaii.edu>
2024-10-30 17:26:51 -1000

    update

---
+++ Lab12/Ex1.py
@@ -0,0 +1,19 @@
+# Open a URL and print some content from it. Print the instances of the <title> tag
+
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = "https://data.cityofchicago.org/Historic-Preservation/Landmark-Districts/zidz-sdfj/about_data"
+encoding = 'utf-8'
+
+# open the web page
+print(f"Opening {url}...")
+webpage = urllib.request.urlopen(url)
+
+# iterate through each line in the webpage and search for <title>
+for line in webpage:
+    line = line.decode(encoding)
+    if '<title>' in line:
+        print(line)
